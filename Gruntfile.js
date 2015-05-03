622d846b8e12612b7b820fc67c7ad56c77349ae8
Michael Bowen <mikee.bowen@gmail.com>
2015-05-03 14:06:33 -0700

    create headerReader.js to read headers and add it to main function in reader.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,13 +4,16 @@ exports = module.exports = {};
 
 module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks("grunt-simple-mocha");
 
+  // initialize grunt
   grunt.initConfig({
+    // create jshint task
     jshint: {
       dev: {
         src: ['Gruntfile.js', 'index.js', 'lib/**/*.js', 'test/**/*.js']
       },
-
+      // bring in options from .jshintrc
       options: {
         shintrc: '.jshintrc'
       }
