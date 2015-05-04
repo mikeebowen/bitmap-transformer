68e56bc034e9cbf30d838c1563246af7348c4dc9
Michael Bowen <mikee.bowen@gmail.com>
2015-05-04 12:01:02 -0700

    make reader_test.js and writer_test.js work

--- test/writer_test.js
+++ test/writer_test.js
@@ -6,19 +6,23 @@ var expect = require('chai').expect;
 // require fs
 var fs = require('fs');
 var writer = require('../lib/writer.js');
-var originalImage = require('../img/bitmap1.bmp');
-// var image = fs.readFile('../new-bit-map.bmp');
+// var originalImage = require('../img/bitmap1.bmp');
+var image = fs.readFile('../new-bit-map.bmp');
 
 
 describe('Test file creation', function () {
-    before(
-    writer.write(originalImage);
-    console.log('the files has been created');
-  );
-  it ('There should be a new file named new-bit-map.bmp', function() {
-    expect(fs.exists('../new-bit-map.bmp')).to.eql(true);
+  before(function (done) {
+    fs.readFile(image, function (error, data) {
+      if (error) {
+        throw error;
+      }
+      writer.write(data);
+      done();
+    });
   });
-})
+  it ('There should be a new file named new-bit-map.bmp', function() {
+    expect(fs.exists('new-bit-map.bmp')).to.eql(true);
+  });
+});
 
-// exports.writer_test = writer_test;
 */
